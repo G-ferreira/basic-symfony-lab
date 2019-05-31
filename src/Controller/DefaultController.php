76f733441de1e4289b5c67b566882ca8a74da575
Gabriel <gabriel.ferreira@justdigital.com.br>
2019-05-31 15:58:44 -0300

    Class 14 - Entities (models)

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        //$users = ['Adam', 'Robert', 'John', 'Susan'];
+        $user = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => $user,
+        ]);
+    }
+}
